3a328024e2ae60c9aaab1875d0b5d32be4d90a2f
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-04-14 13:13:43 +0100

    feat: add no user activities view

--- components/Profile/RecentActivity.tsx
+++ components/Profile/RecentActivity.tsx
@@ -1,4 +1,5 @@
 import PollCard from "../Poll/PollCard";
+import Image from "next/image";
 
 const mockPolls = [
   {
@@ -49,16 +50,37 @@ export default function RecentActivity() {
   return (
     <div className="mb-4">
       <h3 className="text-lg font-medium mb-4 text-primary">Recent Activity</h3>
+      {mockPolls.length === 0 ? (
+        <NoActivitiesView />
+      ) : (
+        <div className="space-y-4">
+          {mockPolls.map((poll) => (
+            <PollCard key={poll.pollId} poll={poll} />
+          ))}
+        </div>
+      )}
 
-      <div className="space-y-4">
-        {mockPolls.map((poll) => (
-          <PollCard key={poll.pollId} poll={poll} />
-        ))}
-      </div>
+      {mockPolls.length > 0 && (
+        <button className="w-full bg-primary text-white font-medium text-lg py-3 rounded-lg mt-4">
+          View all Activities
+        </button>
+      )}
+    </div>
+  );
+}
 
-      <button className="w-full bg-primary text-white font-medium text-lg py-3 rounded-lg mt-4">
-        View all Activities
-      </button>
+function NoActivitiesView() {
+  return (
+    <div className="flex flex-col items-center justify-center rounded-lg mt-16">
+      <Image
+        src="/illustrations/no-polls.svg"
+        alt="No polls found illustration"
+        width={221}
+        height={150}
+      />
+      <p className="text-gray-900 font-medium mt-4">
+        This user has no activities yet
+      </p>
     </div>
   );
 }
