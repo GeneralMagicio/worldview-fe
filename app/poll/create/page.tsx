325996ad0a8e4c4ae7430793c87655ccdaa16df4
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-04-22 11:00:07 +0100

    fix: adjust header for create poll page

--- app/poll/create/page.tsx
+++ app/poll/create/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 export default function CreatePollPage() {
   return (
     <main className="flex-1 bg-white rounded-t-3xl p-5">
-      <Header title="Create a Poll" />
+      <Header title="Create a Poll" isCreatePoll={true} />
       <PollForm />
     </main>
   );
