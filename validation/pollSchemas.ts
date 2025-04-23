f5177c5121091abc05879105b98e63321491bc36
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-04-23 04:56:38 +0100

    fix: update create poll validation schema

--- validation/pollSchemas.ts
+++ validation/pollSchemas.ts
@@ -9,8 +9,8 @@ export const pollSchema = z.object({
       (val) => ({ message: `${val.length}/90 Max character limit reached` })
     )
     .refine(
-      (val) => val.length >= 8,
-      (val) => ({ message: `${val.length}/90` })
+      (val) => val.length >= 10,
+      (val) => ({ message: "Must be at least 10 characters" })
     ),
   description: z
     .string()
@@ -20,14 +20,24 @@ export const pollSchema = z.object({
       (val) => ({ message: `${val.length}/1400 Max character limit reached` })
     ),
   tags: z
-    .array(z.string().nonempty("Tag cannot be empty"))
+    .array(
+      z
+        .string()
+        .nonempty("Tag cannot be empty")
+        .refine(
+          (val) => val.length <= 20,
+          (val) => ({
+            message: `${val.length}/20 Max tag character limit reached`,
+          })
+        )
+        .refine(
+          (val) => val.length >= 3,
+          (val) => ({ message: "Must be at least 3 characters" })
+        )
+    )
     .refine(
       (val) => val.length <= 5,
       (val) => ({ message: `${val.length}/5 Max tag limit reached` })
-    )
-    .refine(
-      (val) => val.length >= 1,
-      (val) => ({ message: `${val.length}/5` })
     ),
   options: z
     .array(
@@ -39,7 +49,11 @@ export const pollSchema = z.object({
           (val) => ({ message: `${val.length}/30 Max character limit reached` })
         )
     )
-    .min(2, "At least 2 options are required"),
+    .min(2, "At least 2 options are required")
+    .refine(
+      (val) => val.length <= 20,
+      (val) => ({ message: `${val.length}/20 Max option limit reached` })
+    ),
   startDate: z.string().nonempty("Start date is required"),
   endDate: z.string().nonempty("End date is required"),
   isAnonymous: z.boolean().optional(),
