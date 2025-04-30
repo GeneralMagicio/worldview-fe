d694023693d054d08df83e9b619bb4bdff2e4076
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-04-30 20:56:04 +0100

    fix: poll creation form ui enhancment

--- components/Poll/PollForm.tsx
+++ components/Poll/PollForm.tsx
@@ -51,7 +51,7 @@ export default function PollForm() {
   } = usePollForm();
 
   const BASE_INPUT_CLASSES =
-    "flex h-12 w-full rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 focus-visible:ring-1 focus-visible:ring-gray-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
+    "flex h-12 w-full rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 focus-visible:border-gray-500 disabled:cursor-not-allowed disabled:opacity-50";
 
   const handleTagChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
