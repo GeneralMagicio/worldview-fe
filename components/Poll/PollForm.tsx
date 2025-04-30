7a9f629ee1003708fe5c64eed170532a70966281
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-04-30 19:55:49 +0100

    fix: remove console.log

--- components/Poll/PollForm.tsx
+++ components/Poll/PollForm.tsx
@@ -50,8 +50,6 @@ export default function PollForm() {
     handlePublish,
   } = usePollForm();
 
-  console.log(errors);
-
   const BASE_INPUT_CLASSES =
     "flex h-12 w-full rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 focus-visible:ring-1 focus-visible:ring-gray-500 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
 
