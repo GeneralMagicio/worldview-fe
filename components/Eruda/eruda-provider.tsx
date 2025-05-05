147ab204d0a7bf3c95c59d3e421f120c0c7777f0
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-05-05 21:19:25 +0100

    Revert "enable eruda"
    This reverts commit 54f0bfbea883a2a1d0d1fa8099e2a577993cfb15.

--- components/Eruda/eruda-provider.tsx
+++ components/Eruda/eruda-provider.tsx
@@ -5,16 +5,16 @@ import { ReactNode, useEffect } from "react";
 
 export const Eruda = (props: { children: ReactNode }) => {
   useEffect(() => {
-    // if (
-    //   typeof window !== "undefined" &&
-    //   process.env.NODE_ENV !== "production"
-    // ) {
-    try {
-      eruda.init();
-    } catch (error) {
-      console.log("Eruda failed to initialize", error);
+    if (
+      typeof window !== "undefined" &&
+      process.env.NODE_ENV !== "production"
+    ) {
+      try {
+        eruda.init();
+      } catch (error) {
+        console.log("Eruda failed to initialize", error);
+      }
     }
-    // }
   }, []);
 
   return <>{props.children}</>;
