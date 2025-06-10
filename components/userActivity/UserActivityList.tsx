7a4e95b383643d705f75a2d2f826dc5eded35ef1
Ramin <raminramazanpour@gmail.com>
2025-06-10 15:55:52 +0330

    add log

--- components/userActivity/UserActivityList.tsx
+++ components/userActivity/UserActivityList.tsx
@@ -91,6 +91,8 @@ export default function UserActivityList({
         index === self.findIndex(t => t.pollId === userAction.pollId),
     )
 
+    console.log('uniqueUserActions', uniqueUserActions)
+
     return (
       <div className="space-y-4">
         {uniqueUserActions.map(userAction => (
